700399625f5e649c01d1ff1cbee3516d44f26aff
yummysushii <tn675411@gmail.com>
2024-09-03 18:15:17 +1000

    Create processing_tri.py

---
+++ processing_tri.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+dataToProcess = pd.read_csv("Melbourne_housing_FULL.csv")
+
+dataToProcess.drop(columns=['Address','Type','Method','SellerG','Date','Distance','Postcode','Car','BuildingArea','YearBuilt','CouncilArea','Lattitude','Longtitude','Regionname','Propertycount','Bedroom2'], inplace=True)
+
+dataToProcess.drop_duplicates(inplace=True)
+
+dataToProcess.rename(columns={'Rooms': 'Number of Bedrooms', 'Price':'Price($)'}, inplace=True)
+
+
+cleanData = dataToProcess.dropna()
+
+print(cleanData)
