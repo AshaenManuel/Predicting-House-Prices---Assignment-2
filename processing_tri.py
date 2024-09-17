63e574dc57ecd784b48cc897771d53b9a762af92
yummysushii <tn675411@gmail.com>
2024-09-17 16:56:03 +1000

    chaned

--- processing_tri.py
+++ processing_tri.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 dataToProcess = pd.read_csv("Melbourne_housing_FULL.csv")
 
-dataToProcess.drop(columns=['Address','Type','Method','SellerG','Date','Distance','Postcode','Car','BuildingArea','YearBuilt','CouncilArea','Lattitude','Longtitude','Regionname','Propertycount','Bedroom2'], inplace=True)
+dataToProcess.drop(columns=['Address','Type','Method','SellerG','Date','Car','BuildingArea','YearBuilt','CouncilArea','Lattitude','Longtitude','Regionname','Propertycount','Bedroom2'], inplace=True)
 
 dataToProcess.drop_duplicates(inplace=True)
 
--- support_vector_regression.py
+++ support_vector_regression.py
@@ -1,4 +1,5 @@
 import processing_tri
+import math
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -11,24 +12,24 @@ df = processing_tri.cleanData
 
 
 # Modify the original DataFrame
-df.loc[:, 'Price($)'] = df['Price($)'] / 1000
+df.loc[:, 'Price($)'] = df['Price($)'] / 1000000
 
 # Scale all features using StandardScaler
 scaler = StandardScaler()
 
 # Define features and target
-features = ['Bathroom', 'Landsize', 'Number of Bedrooms']
+features = ['Bathroom', 'Landsize', 'Number of Bedrooms', 'Distance']
 X = df[features]
 X_scaled = scaler.fit_transform(X)
 
 # Scale the target (Price)
-y = df['Price($)']/1000
+y = df['Price($)']
 
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
 
 # Create and train the linear regression model
-model = svm.LinearSVR()
+model = svm.SVR()
 model.fit(X_train, y_train)
 
 # Predict on test data
@@ -36,4 +37,6 @@ y_pred = model.predict(X_test)
 
 # Print model performance
 print('Mean Squared Error: %.2f' % mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error: %.2f' % math.sqrt(mean_squared_error(y_test, y_pred)))
 print('R^2 Score: %.2f' % r2_score(y_test, y_pred))
+
