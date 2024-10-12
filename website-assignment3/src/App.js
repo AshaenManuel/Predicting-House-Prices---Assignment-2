c23820bf84ba0caba23e806012810a78293dbbca
AshaenManuel <104313773@student.swin.edu.au>
2024-10-13 00:45:54 +1100

    form created

--- website-assignment3/src/App.js
+++ website-assignment3/src/App.js
@@ -1,21 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   AppBar, Toolbar, Typography, Container, Box, Button, Tab, Tabs,
-  TextField, Grid2, Snackbar, Alert, Paper
+  TextField, Snackbar, Alert, Select, MenuItem
 } from '@mui/material';
+import suburbsData from './finaldata.json';
 
 function App() {
   const [value, setValue] = useState(0);
   const [formData, setFormData] = useState({
     suburb: '',
     rooms: '',
+    bedrooms: '',
+    bathrooms: '',
     distance: '',
     yearBuilt: '',
-    price: '',
     population: '',
     educationScore: ''
   });
+  const [suburbs, setSuburbs] = useState([]);
   const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+  useEffect(() => {
+    const suburbList = suburbsData.Sheet1.map((item) => item.Suburb);
+    const uniqueSuburbs = [...new Set(suburbList)].sort();
+    setSuburbs(uniqueSuburbs);
+  }, []);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
@@ -23,11 +32,49 @@ function App() {
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
+
+    if (name === 'rooms' || name === 'bedrooms' || name ==='bathrooms' || name === 'year') {
+      const numericValue = parseInt(value, 10);
+      if (!isNaN(numericValue) && numericValue.toString() === value) {
+        setFormData({ ...formData, [name]: numericValue });
+      } else if (value === '') {
+        setFormData({ ...formData, [name]: '' });
+      }
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
   };
 
   const handleSubmit = () => {
-    // Handle form submission logic here
+    if (formData.rooms < 1 || formData.rooms > 8) {
+      alert("Number of rooms must be between 1 and 8.");
+      return;
+    }
+    if (formData.bedrooms < 0 || formData.bedrooms > 10) {
+      alert("Number of bedrooms must be between 0 and 10.");
+      return;
+    }
+    if (formData.bathrooms < 0 || formData.bathrooms > 6) {
+      alert("Number of bathrooms must be between 0 and 6.");
+      return;
+    }
+    if (formData.distance < 1) {
+      alert("Distance from the CBD must be greater than 0.");
+      return;
+    }
+    if (formData.year > 2024) {
+      alert("Year built must be before this year.");
+      return;
+    }
+    if (formData.population < 1) {
+      alert("Population must be greater than 0.");
+      return;
+    }
+    if (formData.educationScore < 1) {
+      alert("Education Score must be greater than 0.");
+      return;
+    }
+    // Proceed with form submission
     console.log(formData);
     setSnackbarOpen(true);
   };
@@ -83,14 +130,117 @@ function App() {
               component="form"
               sx={{ display: 'flex', flexDirection: 'column', gap: 2, mt: 2 }}
             >
-              <TextField label="Suburb" variant="outlined" />
-              <TextField label="Number of Rooms" variant="outlined" />
-              <TextField label="Distance (km)" variant="outlined" />
-              <TextField label="Year Built" variant="outlined" />
-              <TextField label="Price in $1,000,000" variant="outlined" />
-              <TextField label="Population" variant="outlined" />
-              <TextField label="Education Score" variant="outlined" />
-              <Button variant="contained" sx={{ mt: 2, mb: 4 }}>Predict</Button>
+              <Select
+                value={formData.suburb}
+                onChange={handleInputChange}
+                name="suburb"
+                displayEmpty
+                variant="outlined"
+              >
+                <MenuItem value="" disabled>
+                  Select Suburb
+                </MenuItem>
+                {suburbs.map((suburb, index) => (
+                  <MenuItem key={index} value={suburb}>
+                    {suburb}
+                  </MenuItem>
+                ))}
+              </Select>
+              <TextField
+                label="Number of Rooms"
+                variant="outlined"
+                type="number"
+                name="rooms"
+                value={formData.rooms}
+                onChange={handleInputChange}
+                slotProps={{
+                  htmlInput: {
+                    min: 1,
+                    max: 8,
+                  }
+                }}
+              />
+              <TextField
+                label="Number of Bedrooms"
+                variant="outlined"
+                type="number"
+                name="bedrooms"
+                value={formData.bedrooms}
+                onChange={handleInputChange}
+                slotProps={{
+                  htmlInput: {
+                    min: 0,
+                    max: 10,
+                  }
+                }}
+              />
+              <TextField
+                label="Number of Bathrooms"
+                variant="outlined"
+                type="number"
+                name="bathrooms"
+                value={formData.bathrooms}
+                onChange={handleInputChange}
+                slotProps={{
+                  htmlInput: {
+                    min: 0,
+                    max: 6,
+                  }
+                }}
+              />
+              <TextField
+                label="Distance (km) from CBD"
+                variant="outlined"
+                type="number"
+                name="distance"
+                value={formData.distance}
+                onChange={handleInputChange}
+                slotProps={{
+                  htmlInput: {
+                    min: 1,
+                  }
+                }}
+              />
+              <TextField
+                label="Year Built"
+                variant="outlined"
+                type="number"
+                name="year"
+                value={formData.year}
+                onChange={handleInputChange}
+                slotProps={{
+                  htmlInput: {
+                    max: 2024,
+                  }
+                }}
+              />
+              <TextField
+                label="Population"
+                variant="outlined"
+                type="number"
+                name="population"
+                value={formData.population}
+                onChange={handleInputChange}
+                slotProps={{
+                  htmlInput: {
+                    min: 1,
+                  }
+                }}
+              />
+              <TextField
+                label="Education Score"
+                variant="outlined"
+                type="number"
+                name="educationscore"
+                value={formData.educationScore}
+                onChange={handleInputChange}
+                slotProps={{
+                  htmlInput: {
+                    min: 1,
+                  }
+                }}
+              />
+              <Button variant="contained" sx={{ mt: 2, mb: 4 }} onClick={handleSubmit}>Predict</Button>
             </Box>
           </Box>
         )}
